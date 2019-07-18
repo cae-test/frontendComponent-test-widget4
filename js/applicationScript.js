35ba4c567f35cc25a784c7bed269187788f402fe
CAE service user <cae@cloud10.dbis.rwth-aachen.de>
2019-07-19 00:43:59 +0200

    Update widget.xml

--- js/applicationScript.js
+++ js/applicationScript.js
@@ -40,7 +40,7 @@ var init = function() {
 
   };
 
-  client = new Las2peerWidgetLibrary("http://cloud10.dbis.rwth-aachen.de:8086/images", iwcCallback);
+  client = new Las2peerWidgetLibrary("127.0.0.1/images", iwcCallback);
 
   refreshImages(); 
   document.getElementById("submit-button").onclick = uploadImage;
